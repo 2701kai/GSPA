e799050d5bb4cbd8b2b4cb3ee18b3bf0ae430b28
vvSoft <vvsoft@vvSofts-iMac.local>
2025-05-13 10:49:36 +0200

    Upload ObjPage und Updated App.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import ObjPage from "./pages/ObjPage";
 
 function App() {
-  return <div className="app-container"></div>;
+  return (
+    <div className="app-container">
+      <ObjPage />
+    </div>
+  );
 }
 
 export default App;
