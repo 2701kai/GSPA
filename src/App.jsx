1e3afed296768e94fbf14a9beb4d0a910c784a7e
Felix <payermann@gmail.com>
2025-05-14 09:33:25 +0200

    hotfix route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Series from "./pages/series";
 import Watchlist from "./pages/watchlist";
 import Movie from "./pages/movie";
 import Serie from "./pages/serie";
+import ObjPage from "./pages/ObjPage";
 import "./App.css";
 
 function App() {
@@ -12,16 +13,15 @@ function App() {
     <Router>
       <Navbar />
       <div className="app-container">
-      <Routes>
-  <Route path="/" element={<Movies />} />
-  <Route path="/movies" element={<Movies />} />
-  <Route path="/movies/:id" element={<Movie />} />
-  <Route path="/series/:id" element={<Serie />} />
-  <Route path="/series" element={<Series />} />
-  <Route path="/watchlist" element={<Watchlist />} />
-  <Route path="/testobj" element={<ObjPage />} /> {/* Ekledik */}
-</Routes>
-
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:id" element={<Movie />} />
+          <Route path="/series/:id" element={<Serie />} />
+          <Route path="/series" element={<Series />} />
+          <Route path="/watchlist" element={<Watchlist />} />
+          <Route path="/testobj" element={<ObjPage />} /> {/* Ekledik */}
+        </Routes>
       </div>
     </Router>
   );
