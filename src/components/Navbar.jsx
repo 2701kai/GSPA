6ac7f9c173cced7342443a5926eabc2239c1a432
2701kai <2701kai@gmail.com>
2025-05-15 17:02:08 +0200

    added uttons to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,27 +14,40 @@
 // export default Navbar;
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="bg-base-200 p-4 shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-primary">GSPA</h1>
+        <h1 className="text-2xl font-bold text-primary">GSPA fork</h1>
         <ul className="flex space-x-4">
           <li>
-            <a href="#" className="btn btn-ghost text-lg">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" className="btn btn-ghost text-lg">
+            <Link to="/movies" className="btn btn-ghost bg-orange-900 text-lg">
               Movies
-            </a>
+            </Link>
+            {/* <a href="#" className="btn btn-ghost text-lg">
+              Home
+            </a> */}
           </li>
           <li>
-            <a href="#" className="btn btn-ghost text-lg">
+            <Link to="/series" className="btn btn-ghost bg-orange-900 text-lg">
+              Series
+            </Link>
+            {/* <a href="#" className="btn btn-ghost text-lg">
+              Movies
+            </a> */}
+          </li>
+          <li>
+            <Link
+              to="/watchlist"
+              className="btn btn-ghost bg-orange-900 text-lg"
+            >
+              Watchlist
+            </Link>
+            {/* <a href="#" className="btn btn-ghost text-lg">
               About
-            </a>
+            </a> */}
           </li>
         </ul>
       </div>
