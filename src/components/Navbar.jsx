d26c87b5f36241117fe05f1d913171ab0a5cce5b
2701kai <2701kai@gmail.com>
2025-05-15 17:07:52 +0200

    bugfix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
   return (
     <nav className="bg-base-200 p-4 shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-primary">GSPA fork</h1>
+        <h1 className="text-2xl font-bold text-primary">GSPA</h1>
         <ul className="flex space-x-4">
           <li>
             <Link to="/movies" className="btn btn-ghost bg-orange-900 text-lg">
